6b02a027b748adc4df0c2179d72cdd10a5060bf8
llyxz93 <8679138@qq.com>
2019-08-07 16:38:31 +0800

    修改题目内容

--- src/main/java/lambda_test/ZkgTest.java
+++ src/main/java/lambda_test/ZkgTest.java
@@ -1,6 +1,13 @@
 package lambda_test;
 
+import cn.hutool.core.util.NumberUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 测试
@@ -9,27 +16,10 @@ import java.util.List;
  * @version 1.0
  * @date 2019-08-06 14:57
  */
+@Slf4j
 public class ZkgTest {
 
     public static void main(String[] args) {
-
-        //用户 列表 有10条信息
-        List<User> userList = User.getUserList();
-
-        //任务 列表 有60条信息
-        List<Task> taskList = Task.getTaskList();
-
-
-        // 题目1: 统计 男生 比例
-        // 例子: 2:3, 格式说明: 左边为男生数量,右边为女生数量,中间加冒号
-
-
-        // 题目2: 统计 男生 完成的 任务数量
-        // 例子: 3, 格式说明: 直接显示数字
-
-
-        // 题目3: 显示 完成一半任务及以上的 用户列表
-        // 例子: ["郑坚白",3], 格式说明: 左边为用户名，右边为该用户完成的任务个数
 
 
     }
